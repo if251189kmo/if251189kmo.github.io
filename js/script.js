e29d12d9f55ee455455847e5aa2ab1f606e1a293
if251189kmo <33289750+if251189kmo@users.noreply.github.com>
2018-05-07 23:21:19 +0300

    дописав кномку що вертає на початок сторінки

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(function(){
+
+    var $links = $('.menuItem a');
+    var $mh = parseInt($('.menuItem').height()) + 20;
+    
+    $links.on('click', function(e){
+    	e.preventDefault();
+
+    	$links.removeClass('active');
+    	var $a = $(this).addClass('active');
+    	var selector = $a.attr('href');
+    	var $target = $(selector);
+
+    	if($target.length > 0){
+	    	$('html,body').animate({
+	    		scrollTop: $target.offset().top - $mh
+	    	}, 500);
+	    }
+    });
+  
+    
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 100) {
+            $("button").addClass("buttonActive");
+        } else {            
+            $("button").removeClass("buttonActive");
+        }
+        
+    });
+
+    $("button").click(function(){
+
+        $('html,body').animate({scrollTop: 0});
+
+    });
+
+
+
+
+});
